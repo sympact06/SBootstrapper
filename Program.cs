fbe3ab15bb5038ca15d694856c07e5235430d4c7
Olivier Flentge <femkespa074@gmail.com>
2022-04-07 21:16:08 +0200

    Added Version

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using SBootstrapperHandler;
+
+namespace SBootstrapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SBootstrapperLib SBootstrapper = new SBootstrapperLib();
+            SBootstrapper.Init("https://raw.githubusercontent.com/sympact06/SBootstrapper/master/test.txt", "https://mirrorhosting.nl/images.zip", "Olivier Flentge","no.exe");
+
+           
+
+        }
+    }
+}
